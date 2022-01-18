f169872e61fcd45f42679c59ba77b20c13975eef
salonisingh77 <85999118+salonisingh77@users.noreply.github.com>
2022-01-18 23:57:40 +0530

    Create palindrome_string.py
    Check a string is palindrome or not

---
+++ palindrome_string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, S):
+        r=S[::-1]
+        if(r==S):
+            return 1
+        return 0# code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+        S = input()
+        ob = Solution()
+        answer = ob.isPalindrome(S)
+        print(answer)
+
+# } Driver Code Ends
